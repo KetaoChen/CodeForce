26814055f63e263d3afb04a5d75e8594cdba1920
KetaoChen <kchen343@gatech.edu>
2020-01-17 00:07:30 -0500

    combo

--- src/Two_Arrays_1288C.java
+++ src/Two_Arrays_1288C.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class Two_Arrays {
+public class Two_Arrays_1288C {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
         System.out.println(getRes(in));

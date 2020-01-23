4c932296b33ebbee4beebd1d230748efc1bd021b
KetaoChen <kchen343@gatech.edu>
2020-01-23 15:11:48 -0500

    fix template

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,212 @@
+import java.io.*;
 import java.util.*;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
- 
-public class Main {
-    
-    public static void main(String[] args) {
-        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int n = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
-        }
-        System.out.println(getRes(arr, 0));
-    }
+import java.lang.*;
 
-    private static int getRes(int[] arr, int m) {
-        Arrays.sort(arr);
-        long sum = 0;
-        int mod = 1000000007;
-        int l = arr.length;
-        long base = 1;
 
-        for (int i = 0; i <= l - m; i++) {
-            //System.out.println(arr[m + i - 1] + " " + arr[l - m - i] + " " + base);
-            sum = (sum + base * arr[m + i - 1] - base * arr[l - m - i]) % mod;
-            while (base % (i + 1) != 0) {
-                base += mod;
+public class Main implements Runnable
+{
+    @Override
+    public void run() {
+        InputReader in = new InputReader(System.in);
+        PrintWriter w = new PrintWriter(System.out);
+        int q = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
+        int x = in.nextInt();
+
+        int[] count = new int[x];
+        int start = 0;
+
+        for (int i = 0; i < q; i++) {
+            count[in.nextInt() % x]++;
+            while (count[start % x] > (start / x)) {
+                start++;
             }
-            base = (base * (m + i) / (i + 1)) % mod;
+            w.println(start);
+        }
+        w.flush();
+        w.close();
+    }
+
+    static class InputReader
+    {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public InputReader(InputStream stream)
+        {
+            this.stream = stream;
         }
 
-        return (int) ((sum + mod) % mod);
+        public int read()
+        {
+            if (numChars==-1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars)
+            {
+                curChar = 0;
+                try
+                {
+                    numChars = stream.read(buf);
+                }
+                catch (IOException e)
+                {
+                    throw new InputMismatchException();
+                }
+
+                if(numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        public int nextInt()
+        {
+            int c = read();
+
+            while(isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do
+            {
+                if(c<'0'||c>'9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do
+            {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.')
+            {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.')
+            {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c))
+                {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do
+            {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c)
+        {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next()
+        {
+            return readString();
+        }
+
+        public interface SpaceCharFilter
+        {
+            public boolean isSpaceChar(int ch);
+        }
     }
+
+    public static void main(String args[]) throws Exception
+    {
+        new Thread(null, new Main(),"Main",1<<27).start();
+    }
+
 }

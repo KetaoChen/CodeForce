fa97b6d3999b31faa3ee467f9828aa261ba9cb54
KetaoChen <kchen343@gatech.edu>
2020-07-24 15:37:43 -0700

    Div1 659

---
+++ src/Div1_659/A_Graph_Greedy.java
@@ -0,0 +1,241 @@
+package Div1_659;
+
+import java.io.*;
+import java.util.InputMismatchException;
+
+
+public class A_Graph_Greedy implements Runnable
+{
+    @Override
+    public void run() {
+        InputReader in = new InputReader(System.in);
+        w = new PrintWriter(System.out);
+        t = Integer.parseInt(in.nextLine());
+        for (int i = 0; i < t; i++) {
+            n = Integer.parseInt(in.nextLine());
+            s1 = in.nextLine();
+            s2 = in.nextLine();
+            getRes();
+        }
+
+        w.flush();
+        w.close();
+    }
+
+    static PrintWriter w;
+    static int t, k, n;
+    static Integer[] arr;
+    static String s1, s2;
+
+    private static void getRes() {
+        int[][] count = new int[20][20];
+        for (int i = 0; i < n; i++) {
+            int a = s1.charAt(i) - 'a', b = s2.charAt(i) - 'a';
+            if (b < a) {
+                w.println(-1);
+                return;
+            }
+            count[a][b]++;
+        }
+        int res = 0;
+        for (int i = 0; i < 20; i++) {
+            int j  = i + 1;
+            while (j < 20) {
+                if (count[i][j] > 0) {
+                    res++;
+                    break;
+                }
+                j++;
+            }
+            int col = j;
+            while (j < 20) {
+                count[col][j] += count[i][j++];
+            }
+        }
+        w.println(res);
+    }
+
+    static class InputReader
+    {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public InputReader(InputStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public int read()
+        {
+            if (numChars==-1)
+                throw new InputMismatchException();
+
+            if (curChar >= numChars)
+            {
+                curChar = 0;
+                try
+                {
+                    numChars = stream.read(buf);
+                }
+                catch (IOException e)
+                {
+                    throw new InputMismatchException();
+                }
+
+                if(numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        public int nextInt()
+        {
+            int c = read();
+
+            while(isSpaceChar(c))
+                c = read();
+
+            int sgn = 1;
+
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+
+            int res = 0;
+            do
+            {
+                if(c<'0'||c>'9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res * sgn;
+        }
+
+        public long nextLong()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+
+            do
+            {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public double nextDouble()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-')
+            {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.')
+            {
+                if (c == 'e' || c == 'E')
+                    return res * Math.pow(10, nextInt());
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.')
+            {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c))
+                {
+                    if (c == 'e' || c == 'E')
+                        return res * Math.pow(10, nextInt());
+                    if (c < '0' || c > '9')
+                        throw new InputMismatchException();
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        public String readString()
+        {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do
+            {
+                res.appendCodePoint(c);
+                c = read();
+            }
+            while (!isSpaceChar(c));
+
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c)
+        {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next()
+        {
+            return readString();
+        }
+
+        public interface SpaceCharFilter
+        {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    public static void main(String args[]) throws Exception
+    {
+        new Thread(null, new A_Graph_Greedy(),"Main",1<<27).start();
+    }
+
+}
